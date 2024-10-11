8927d898cb7744ad9060c113fef44ec65b8b4240
MuhammadShayan404 <141303510+DevShayan404@users.noreply.github.com>
2024-10-11 15:54:52 +0500

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  startYear: number = 1989;
+  yearsOfWork!: number;
+  ngOnInit() {
+    const currentYear = new Date().getFullYear();
+    this.yearsOfWork = currentYear - this.startYear;
+  }
 }
