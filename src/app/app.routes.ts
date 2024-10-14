aeb7c33d06200b648f7926505151a1ac019cd3be
MuhammadShayan404 <141303510+DevShayan404@users.noreply.github.com>
2024-10-14 12:16:25 +0500

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,25 @@ export const routes: Routes = [
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'about-us',
+    loadComponent: () =>
+      import('./pages/about-us/about-us.component').then(
+        (c) => c.AboutUsComponent
+      ),
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () =>
+      import('./pages/contact-us/contact-us.component').then(
+        (c) => c.ContactUsComponent
+      ),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./pages/products/products.component').then(
+        (c) => c.ProductsComponent
+      ),
+  },
 ];
--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +10,57 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  navLinks: string[] = ['Home', 'About', 'Product', 'Contact'];
+  navLinks: string[] = ['Home', 'About Us', 'Products', 'Contact Us'];
+  activeLink: number = 0;
+
+  constructor(private router: Router, private location: Location) {}
+
+  ngOnInit() {
+    const url = this.location.path();
+    this.setActiveLinkByUrl(url);
+  }
+
+  setActiveLink(i: number, name: string) {
+    switch (name) {
+      case 'Home':
+        this.router.navigate(['']);
+        this.activeLink = i;
+        break;
+      case 'About Us':
+        this.router.navigate(['about-us']);
+        this.activeLink = i;
+        break;
+      case 'Products':
+        this.router.navigate(['products']);
+        this.activeLink = i;
+        break;
+      case 'Contact Us':
+        this.router.navigate(['contact-us']);
+        this.activeLink = i;
+        break;
+
+      default:
+        break;
+    }
+  }
+  setActiveLinkByUrl(url: string) {
+    console.log(url);
+    switch (url) {
+      case ' ':
+        this.activeLink = 0;
+        break;
+      case '/about-us':
+        this.activeLink = 1;
+        break;
+      case '/products':
+        this.activeLink = 2;
+        break;
+      case '/contact-us':
+        this.activeLink = 3;
+        break;
+      default:
+        this.activeLink = 0;
+        break;
+    }
+  }
 }
