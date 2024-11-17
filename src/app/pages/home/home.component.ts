c7f7c43b06f7b3e7a78cf3a8e1e79c4c2fd483d7
Muhammad Shayan <141303510+DevShayan404@users.noreply.github.com>
2024-11-17 17:11:02 +0500

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -14,5 +14,25 @@ export class HomeComponent {
   ngOnInit() {
     const currentYear = new Date().getFullYear();
     this.yearsOfWork = currentYear - this.startYear;
+  }
+
+  improvingBrand: number = 0;
+  gettingTraffic: number = 0;
+  targetRelevance: number = 0;
+  increaseTrust: number = 0;
+  activeDefer: boolean = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.scrollY;
+    const scrollTopButtonVisibility = yOffset > 3850;
+    if (scrollTopButtonVisibility) {
+      this.improvingBrand = 91;
+      this.gettingTraffic = 85;
+      this.targetRelevance = 89;
+      this.increaseTrust = 94;
+    }
+    if (yOffset > 10) {
+      this.activeDefer = true;
+    }
   }
 }
