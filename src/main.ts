dd1001f2d6797240f79cbe63c491099302bda9bf
Muhammad Shayan <141303510+DevShayan404@users.noreply.github.com>
2024-11-17 16:58:33 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err))
+  .then(() => {
+    const body = document.querySelector('body');
+    setTimeout(() => {
+      if (body) {
+        body.classList.add('app-loaded');
+      }
+    }, 3000);
+  });
